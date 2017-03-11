e9a53736ee2219ac12254a5c7c40fd6c723b3583
Fenhl <fenhl@fenhl.net>
2017-03-11 16:19:08 +0000

    Print response text for RequestException

--- night-device-report.py
+++ night-device-report.py
@@ -12,9 +12,16 @@ import platform
 import requests
 import subprocess
 
-config = basedir.config_dirs('fenhl/night.json').json()
-device_key = config['deviceKey']
-hostname = config.get('hostname', platform.node().split('.')[0])
+def night_excepthook(type, value, traceback):
+    sys.__excepthook__(type, value, traceback)
+    if issubclass(type, requests.RequestException) and value.response is not None:
+        print('\n' + value.response.text, file=sys.stderr)
+
+sys.excepthook = night_excepthook
+
+CONFIG = basedir.config_dirs('fenhl/night.json').json()
+DEVICE_KEY = CONFIG['deviceKey']
+HOSTNAME = CONFIG.get('hostname', platform.node().split('.')[0])
 
 data = {}
 
@@ -25,7 +32,7 @@ for line in subprocess.check_output(['/usr/sbin/needrestart', '-b'], stderr=subp
 else:
     data['needrestart'] = None
 
-response = requests.post('https://v2.nightd.fenhl.net/dev/{}/report'.format(hostname), json={'args': [device_key], 'data': data}, timeout=60.05)
+response = requests.post('https://v2.nightd.fenhl.net/dev/{}/report'.format(HOSTNAME), json={'args': [DEVICE_KEY], 'data': data}, timeout=60.05)
 response.raise_for_status()
 j = response.json()
 if 'text' in j:
