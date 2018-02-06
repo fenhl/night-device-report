8435bb3545a8d3739efe5ced598efbb6509aae19
Fenhl <fenhl@fenhl.net>
2018-02-06 21:29:23 +0000

    Increase timeout to 10 minutes

--- night-device-report.py
+++ night-device-report.py
@@ -67,5 +67,5 @@ except FileNotFoundError:
 
 # send data
 
-response = requests.post('https://v3.nightd.fenhl.net/device-report/{}'.format(HOSTNAME), json=data, timeout=60.05)
+response = requests.post('https://v3.nightd.fenhl.net/device-report/{}'.format(HOSTNAME), json=data, timeout=600)
 response.raise_for_status()
