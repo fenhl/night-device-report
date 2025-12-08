e4acdf50196785cba17c7b70f0192f810867f71a
Fenhl <fenhl@fenhl.net>
2025-12-08 14:54:10 +0000

    Allow creating report data from library

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
-#![allow(unused_crate_dependencies)] // combined lib/bin crate
-
 use {
     std::{
         cmp::Ordering::*,
+        ffi::OsString,
         io::prelude::*,
+        path::Path,
+        process::Stdio,
     },
+    clap as _, // only used in bin target
+    futures::stream::TryStreamExt as _,
+    gethostname::gethostname,
     gix_hash::ObjectId,
     lazy_regex::regex_captures,
     std::collections::HashMap,
@@ -13,17 +17,116 @@ use {
         Deserialize,
         Serialize,
     },
+    systemstat::{
+        Platform as _,
+        System,
+    },
     tokio::{
-        io,
+        io::{
+            self,
+            AsyncBufReadExt as _,
+            BufReader,
+        },
         process::Command,
     },
+    tokio_stream::wrappers::LinesStream,
     unicode_width::UnicodeWidthStr as _,
-    wheel::traits::{
-        AsyncCommandOutputExt as _,
-        IoResultExt as _,
+    wheel::{
+        fs::{
+            self,
+            File,
+        },
+        traits::{
+            AsyncCommandOutputExt as _,
+            IoResultExt as _,
+        },
     },
 };
-#[cfg(windows)] use wheel::traits::CommandExt as _;
+#[cfg(unix)] use std::path::PathBuf;
+#[cfg(windows)] use {
+    directories::ProjectDirs,
+    wheel::traits::CommandExt as _,
+};
+
+#[derive(Debug, thiserror::Error)]
+pub enum ConfigError {
+    #[error(transparent)] Wheel(#[from] wheel::Error),
+    #[cfg(unix)]
+    #[error("config file not found")]
+    Missing {
+        config_home: Option<PathBuf>,
+        config_dirs: Vec<PathBuf>,
+    },
+    #[cfg(windows)]
+    #[error("failed to find project folder")]
+    ProjectDirs,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error(transparent)] CargoUpdateCheck(#[from] CargoUpdateCheckError),
+    #[error(transparent)] Config(#[from] ConfigError),
+    #[error(transparent)] ParseInt(#[from] std::num::ParseIntError),
+    #[error(transparent)] Reqwest(#[from] reqwest::Error),
+    #[error(transparent)] TryFromInt(#[from] std::num::TryFromIntError),
+    #[error(transparent)] Utf8(#[from] std::string::FromUtf8Error),
+    #[error(transparent)] Wheel(#[from] wheel::Error),
+    #[error("non-UTF-8 string")]
+    OsString(OsString),
+}
+
+impl From<OsString> for Error {
+    fn from(value: OsString) -> Self {
+        Self::OsString(value)
+    }
+}
+
+fn make_true() -> bool { true }
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Config {
+    pub device_key: String,
+    pub hostname: Option<String>,
+    /// Whether I have root access on this device.
+    /// If `true`, night-device-report assumes it is running as `root`.
+    /// If `false`, night-device-report skips checks for system updates which should be handled by root.
+    #[serde(default = "make_true")]
+    pub root: bool,
+}
+
+impl Config {
+    pub async fn load() -> Result<Self, ConfigError> {
+        #[cfg(unix)] {
+            let base_dirs = xdg::BaseDirectories::new();
+            if let Some(path) = base_dirs.find_config_file("fenhl/night.json") {
+                Ok(fs::read_json(path).await?)
+            } else {
+                Err(ConfigError::Missing {
+                    config_home: base_dirs.get_config_home(),
+                    config_dirs: base_dirs.get_config_dirs(),
+                })
+            }
+        }
+        #[cfg(windows)] {
+            let path = ProjectDirs::from("net", "Fenhl", "Night").ok_or(ConfigError::ProjectDirs)?.config_dir().join("config.json");
+            Ok(fs::read_json(path).await?)
+        }
+    }
+
+    pub fn hostname(&self) -> Result<String, OsString> {
+        Ok(if let Some(ref hostname) = self.hostname {
+            hostname.clone()
+        } else {
+            let full_hostname = gethostname().into_string()?;
+            if let Some((prefix, _)) = full_hostname.split_once('.') {
+                prefix.to_owned()
+            } else {
+                full_hostname
+            }
+        })
+    }
+}
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 #[cfg_attr(feature = "async-proto", derive(async_proto::Protocol))]
@@ -38,6 +141,71 @@ pub struct ReportData {
     pub inodes_free: u64,
     pub needrestart: Option<u8>,
     pub oldconffiles: HashMap<String, bool>,
+}
+
+impl ReportData {
+    pub async fn new(config: &Config) -> Result<Self, Error> {
+        let (cargo_updates, cargo_updates_git) = if let Some((cargo_updates, cargo_updates_git)) = check_cargo_updates().await? {
+            (Some(cargo_updates), Some(cargo_updates_git))
+        } else {
+            (None, None)
+        };
+        let fs = System::new().mount_at("/").at("/")?;
+        Ok(Self {
+            cron_apt: config.root && if let os_info::Type::NixOS = os_info::get().os_type() {
+                false // updates are configured to be installed automatically
+            } else {
+                // not NixOS, assume Debian
+                let mut cron_apt = false;
+                let syslogs = vec![Path::new("/var/log/syslog"), Path::new("/var/log/syslog.1")];
+                'cron_apt: for log_path in syslogs {
+                    if log_path.exists() {
+                        let log_f = BufReader::new(File::open(log_path).await?);
+                        for line in LinesStream::new(log_f.lines()).try_collect::<Vec<_>>().await.at(log_path)?.into_iter().rev() {
+                            if line.contains("cron-apt: Download complete and in download only mode") {
+                                cron_apt = true;
+                                break 'cron_apt
+                            } else if line.contains("cron-apt: 0 upgraded, 0 newly installed, 0 to remove and 0 not upgraded.") {
+                                break 'cron_apt
+                            }
+                        }
+                    }
+                }
+                cron_apt
+            },
+            diskspace_total: fs.total.as_u64(),
+            diskspace_free: fs.avail.as_u64(),
+            inodes_total: fs.files_total.try_into()?,
+            inodes_free: fs.files_avail.try_into()?,
+            needrestart: if config.root {
+                match os_info::get().os_type() { // emulate NEEDRESTART-KSTA codes
+                    os_info::Type::Macos => Some(1), // update workflow includes reboot
+                    os_info::Type::NixOS => match Command::new("nixos-needsreboot").status().await.at_command("nixos-needsreboot")?.code() {
+                        Some(0) => Some(1), // no reboot needed
+                        Some(2) => Some(2), // reboot needed //TODO use 3 if it's specifically for a new kernel version (check stderr)
+                        code => {
+                            if let Some(code) = code {
+                                eprintln!("nixos-needsreboot exited with status code {code}");
+                            } else {
+                                eprintln!("nixos-needsreboot exited with no status code");
+                            }
+                            Some(0) // unknown status
+                        }
+                    },
+                    _ => String::from_utf8(Command::new("/usr/sbin/needrestart").arg("-b").stderr(Stdio::null()).output().await.at_command("needrestart")?.stdout)?.lines()
+                        .find_map(|line| line.strip_prefix("NEEDRESTART-KSTA: "))
+                        .map(|line| line.parse())
+                        .transpose()?,
+                }
+            } else { None },
+            oldconffiles: {
+                ["fenhl", "pi"].into_iter()
+                    .map(|username| (username.into(), Path::new("/home").join(username).join("oldconffiles").exists()))
+                    .collect()
+            },
+            cargo_updates, cargo_updates_git,
+        })
+    }
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -102,7 +270,7 @@ pub async fn check_cargo_updates() -> Result<Option<(HashMap<String, [Version; 2
         Err(wheel::Error::CommandExit { output, .. }) if output.status.code().is_some_and(|code| code == 101) => return Ok(None), // `cargo install-update` not installed
         Err(e) => return Err(e.into()),
     };
-    let mut lines = output.stdout.lines();
+    let mut lines = BufRead::lines(&*output.stdout);
     let (package_width, installed_width, latest_width) = loop {
         let line = lines.next().ok_or(CargoUpdateCheckError::MissingTableHeader)?.at_command("cargo install-update")?;
         if let Some((_, package, installed, latest)) = regex_captures!("^(Package +)(Installed +)(Latest +)Needs update$", &line) {
--- src/main.rs
+++ src/main.rs
@@ -4,120 +4,17 @@ use {
     std::{
         env,
         ffi::OsString,
-        num::ParseIntError,
-        path::Path,
-        process::Stdio,
-        string,
         time::Duration,
     },
-    futures::stream::TryStreamExt as _,
-    gethostname::gethostname,
-    serde::{
-        Deserialize,
-        Serialize,
+    serde::Serialize,
+    tokio::process::Command,
+    wheel::traits::IoResultExt as _,
+    night_device_report::{
+        Config,
+        Error,
+        ReportData,
     },
-    systemstat::{
-        Platform as _,
-        System,
-    },
-    tokio::{
-        io::{
-            AsyncBufReadExt as _,
-            BufReader,
-        },
-        process::Command,
-    },
-    tokio_stream::wrappers::LinesStream,
-    wheel::{
-        fs::{
-            self,
-            File,
-        },
-        traits::IoResultExt as _,
-    },
-    night_device_report::ReportData,
 };
-#[cfg(feature = "async-proto")] use async_proto as _; // only used in lib target
-#[cfg(unix)] use std::path::PathBuf;
-#[cfg(windows)] use directories::ProjectDirs;
-
-#[derive(Debug, thiserror::Error)]
-enum Error {
-    #[error(transparent)] CargoUpdateCheck(#[from] night_device_report::CargoUpdateCheckError),
-    #[error(transparent)] Config(#[from] ConfigError),
-    #[error(transparent)] ParseInt(#[from] ParseIntError),
-    #[error(transparent)] Reqwest(#[from] reqwest::Error),
-    #[error(transparent)] TryFromInt(#[from] std::num::TryFromIntError),
-    #[error(transparent)] Utf8(#[from] string::FromUtf8Error),
-    #[error(transparent)] Wheel(#[from] wheel::Error),
-    #[error("non-UTF-8 string")]
-    OsString(OsString),
-}
-
-impl From<OsString> for Error {
-    fn from(value: OsString) -> Self {
-        Self::OsString(value)
-    }
-}
-
-#[derive(Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct Config {
-    device_key: String,
-    hostname: Option<String>,
-    /// Whether I have root access on this device.
-    /// If `true`, night-device-report assumes it is running as `root`.
-    /// If `false`, night-device-report skips checks for system updates which should be handled by root.
-    #[serde(default = "make_true")]
-    root: bool,
-}
-
-#[derive(Debug, thiserror::Error)]
-enum ConfigError {
-    #[error(transparent)] Wheel(#[from] wheel::Error),
-    #[cfg(unix)]
-    #[error("config file not found")]
-    Missing {
-        config_home: Option<PathBuf>,
-        config_dirs: Vec<PathBuf>,
-    },
-    #[cfg(windows)]
-    #[error("failed to find project folder")]
-    ProjectDirs,
-}
-
-impl Config {
-    async fn load() -> Result<Self, ConfigError> {
-        #[cfg(unix)] {
-            let base_dirs = xdg::BaseDirectories::new();
-            if let Some(path) = base_dirs.find_config_file("fenhl/night.json") {
-                Ok(fs::read_json(path).await?)
-            } else {
-                Err(ConfigError::Missing {
-                    config_home: base_dirs.get_config_home(),
-                    config_dirs: base_dirs.get_config_dirs(),
-                })
-            }
-        }
-        #[cfg(windows)] {
-            let path = ProjectDirs::from("net", "Fenhl", "Night").ok_or(ConfigError::ProjectDirs)?.config_dir().join("config.json");
-            Ok(fs::read_json(path).await?)
-        }
-    }
-
-    fn hostname(&self) -> Result<String, OsString> {
-        Ok(if let Some(ref hostname) = self.hostname {
-            hostname.clone()
-        } else {
-            let full_hostname = gethostname().into_string()?;
-            if let Some((prefix, _)) = full_hostname.split_once('.') {
-                prefix.to_owned()
-            } else {
-                full_hostname
-            }
-        })
-    }
-}
 
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -125,8 +22,6 @@ struct CronReport {
     key: String,
     status: Option<i32>,
 }
-
-fn make_true() -> bool { true }
 
 #[derive(clap::Parser)]
 #[clap(version)]
@@ -158,66 +53,7 @@ async fn main(args: Args) -> Result<(), Error> {
             .send().await?
             .error_for_status()?;
     } else {
-        let (cargo_updates, cargo_updates_git) = if let Some((cargo_updates, cargo_updates_git)) = night_device_report::check_cargo_updates().await? {
-            (Some(cargo_updates), Some(cargo_updates_git))
-        } else {
-            (None, None)
-        };
-        let fs = System::new().mount_at("/").at("/")?;
-        let data = ReportData {
-            cron_apt: config.root && if let os_info::Type::NixOS = os_info::get().os_type() {
-                false // updates are configured to be installed automatically
-            } else {
-                // not NixOS, assume Debian
-                let mut cron_apt = false;
-                let syslogs = vec![Path::new("/var/log/syslog"), Path::new("/var/log/syslog.1")];
-                'cron_apt: for log_path in syslogs {
-                    if log_path.exists() {
-                        let log_f = BufReader::new(File::open(log_path).await?);
-                        for line in LinesStream::new(log_f.lines()).try_collect::<Vec<_>>().await.at(log_path)?.into_iter().rev() {
-                            if line.contains("cron-apt: Download complete and in download only mode") {
-                                cron_apt = true;
-                                break 'cron_apt
-                            } else if line.contains("cron-apt: 0 upgraded, 0 newly installed, 0 to remove and 0 not upgraded.") {
-                                break 'cron_apt
-                            }
-                        }
-                    }
-                }
-                cron_apt
-            },
-            diskspace_total: fs.total.as_u64(),
-            diskspace_free: fs.avail.as_u64(),
-            inodes_total: fs.files_total.try_into()?,
-            inodes_free: fs.files_avail.try_into()?,
-            needrestart: if config.root {
-                match os_info::get().os_type() { // emulate NEEDRESTART-KSTA codes
-                    os_info::Type::Macos => Some(1), // update workflow includes reboot
-                    os_info::Type::NixOS => match Command::new("nixos-needsreboot").status().await.at_command("nixos-needsreboot")?.code() {
-                        Some(0) => Some(1), // no reboot needed
-                        Some(2) => Some(2), // reboot needed //TODO use 3 if it's specifically for a new kernel version (check stderr)
-                        code => {
-                            if let Some(code) = code {
-                                eprintln!("nixos-needsreboot exited with status code {code}");
-                            } else {
-                                eprintln!("nixos-needsreboot exited with no status code");
-                            }
-                            Some(0) // unknown status
-                        }
-                    },
-                    _ => String::from_utf8(Command::new("/usr/sbin/needrestart").arg("-b").stderr(Stdio::null()).output().await.at_command("needrestart")?.stdout)?.lines()
-                        .find_map(|line| line.strip_prefix("NEEDRESTART-KSTA: "))
-                        .map(|line| line.parse())
-                        .transpose()?,
-                }
-            } else { None },
-            oldconffiles: {
-                ["fenhl", "pi"].into_iter()
-                    .map(|username| (username.into(), Path::new("/home").join(username).join("oldconffiles").exists()))
-                    .collect()
-            },
-            cargo_updates, cargo_updates_git,
-        };
+        let data = ReportData::new(&config).await?;
         client.post(&format!("https://night.fenhl.net/dev/{}/device-report", config.hostname()?))
             .bearer_auth(&config.device_key)
             .json(&data)
